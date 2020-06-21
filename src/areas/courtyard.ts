063fb67d8adbb5dc3246096de15374f6880b5fa9
Rusty <rusty@rustys-mbp.lan>
2020-06-21 07:26:21 -0500

    putting everything in main for now -- functional first, elegant later

--- src/areas/courtyard.ts
+++
@@ -1,7 +0,0 @@
-import Location from "../engine/Location";
-
-const start = new Location()
-    .setId("Courtyard")
-    .setDesc("You are in the courtyard of Casa Cantera.");
-
-export default start;
--- src/areas/main.ts
+++ src/areas/main.ts
@@ -1,8 +1,25 @@
 import GameEngine from '../engine/GameEngine';
-import startArea from './courtyard';
+import Location from "../engine/Location";
 
-const gameEngine = new GameEngine(startArea);
-gameEngine.setStartLocation(startArea);
+// declarations
+const courtyard = new Location();
+const livingroom = new Location();
+
+// locations
+courtyard
+    .setId("Courtyard")
+    .setDesc("You are in the courtyard of Casa Cantera. You can almost feel the rosemary pollen coat your slightly sweating skin as you listen to the sounds of tepid water trickling through the slightly-functioning fountain. To the north is the door into the Casa.\n\nThere appears to be a note taped to the door.")
+    .link("north", livingroom)
+    .link("through door", livingroom);
+
+livingroom
+    .setId("Living Room")
+    .setDesc("This is the living room.")
+    .link("south", courtyard)
+    .link("through door", courtyard);
+
+const gameEngine = new GameEngine(courtyard);
+gameEngine.setStartLocation(courtyard);
 
 const startTime = new Date();
 function timeDiffInSeconds(): number {
