5056203c21d2573425fb1fd2d911c967a7d9b01d
Rusty Bentley <mycoding.alert@gmail.com>
2020-06-22 18:20:34 -0500

    adds bunkhouse stub, notes for other areas

--- src/areas/rugroom.ts
+++ src/areas/rugroom.ts
@@ -4,4 +4,11 @@ const rugroom = new Location()
     .setId("Cowskin Rug Bedroom")
     .setDesc("This bedroom was dubbed The Cowskin Rug Bedroom due to its floors being covered in cowskin rugs. The door to the east leads back to the hallway.");
 
+/*
+rugs
+deer head statue
+bookcase
+ottoman
+*/
+
 export default rugroom;
--- src/areas/undecidedroom.ts
+++ src/areas/undecidedroom.ts
@@ -4,4 +4,9 @@ const undecidedroom = new Location()
     .setId("Undecided Bedroom")
     .setDesc("This bedroom was dubbed The Undecided Bedroom due to its wall art -- four prints stating 'Yes', 'No', 'Maybe', and 'OK'. The door to the north leads back to the hallway.");
 
+/*
+chest against wall
+bookcase
+*/
+
 export default undecidedroom;
