a1b4b9a29b81a570cd213135b2e4d327a06c83b8
Rusty Bentley <mycoding.alert@gmail.com>
2020-06-22 08:48:17 -0500

    begins some sanity separation

---
+++ src/areas/courtyard.ts
@@ -0,0 +1,7 @@
+import Location from "../engine/Location";
+import Item from '../engine/Item';
+
+export const courtyard = new Location()
+    .setId("Courtyard")
+    .setDesc("You are in the courtyard of Casa Cantera. You can almost feel the rosemary pollen coat your slightly sweating skin as you listen to the sounds of tepid water trickling through the slightly-functioning fountain. To the north is the door into the Casa.\n\nThere appears to be a note taped to the door.");
+
